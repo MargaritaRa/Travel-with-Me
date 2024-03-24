82fb4dcd30df82b3cb9901a4ee54c0f72dddbaae
Margarita Ramirez <margarita26.mr@gmail.com>
2024-03-23 22:13:15 -0400

    added event listener submite and mouse

--- index.js
+++ index.js
@@ -29,3 +29,31 @@ function displayCountriesDetails(country){
     document.querySelector("#countries-passportVisa").textContent = country.passportVisa;
     document.querySelector("#countries-food").textContent = country.food;
 }
+
+let form = document.querySelector("form")
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    buildToDo(event.target.new_todo.value)
+    form.reset()
+})
+
+function buildToDo(todo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = "x"
+    p.textContent = `${todo} `
+    p.appendChild(btn)
+    document.querySelector('#todo_container').appendChild(p)
+}
+
+function handleDelete(event) {
+    event.target.parentNode.remove()
+}
+
+document.addEventListener('mouseenter', event => {
+    document.getElementById("instructions").style.color = "blue";
+});
+document.addEventListener('mouseleave', event => {
+    document.getElementById("instructions").style.color = "black";
+});
